b6f0a2f87ab6d940bfb1b6dd06d1d5962726889e
Stephen McLean <smclean@guidewire.com>
2019-03-21 10:42:02 +0000

    Initial styling commit + nav component.

--- src/app/platform/nav/nav.component.ts
+++ src/app/platform/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faDollarSign, faLightbulb } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  faDollarSign = faDollarSign;
+  faLightbulb = faLightbulb;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleCurrency() {
+    console.log('toggle currency');
+  }
+
+  toggleTheme() {
+    console.log('toggle theme');
   }
 
 }
--- src/app/platform/platform.module.ts
+++ src/app/platform/platform.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [NavComponent],
   exports: [NavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ]
 })
 export class PlatformModule { }
